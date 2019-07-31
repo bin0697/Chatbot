98fa0ce2c9d06e0f7d8665f43fe5dd68b7febe16
bin0697 <37770758+bin0697@users.noreply.github.com>
2019-07-31 11:44:33 +0700

    Add files via upload

--- flask-chat-app-article-master/quicktest.py
+++ flask-chat-app-article-master/quicktest.py
@@ -10,22 +10,39 @@
 
 
 
-# data = {
-#     "cars": {
-#         "Nissan": [
-#             {"model":"Sentra", "doors":4},
-#             {"model":"Maxima", "doors":4},
-#             {"model":"Skyline", "doors":2}
-#         ],
-#         "Ford": [
-#             {"model":"Taurus", "doors":4},
-#             {"model":"Escort", "doors":4}
-#         ]
-#     }
-# }
-
-# data['cars']["Nissan"].append({"model" + "tesst":"simp", "doors":12})
-# print (data['cars']["Nissan"][3]  )
+data = {
+    "cars": {
+        "Nissan": [
+            {"model":"Sentra", "doors":4},
+            {"model":"Maxima", "doors":4},
+            {"model":"Skyline", "doors":2}
+        ],
+        "Ford": [
+            {"model":"Taurus", "doors":4},
+            {"model":"Escort", "doors":4}
+        ]
+    },
+    "sth": ["hâhhaa","tesing"],
+    'sth2': [1,2,3]
+}
+data2 = {
+	'ha': "haha"
+}
+data2['ho'] = "hôhho"
+print (data2)
+# print (len(data['sth']))
+# if len (data['sth']) == 1:
+# 	print("ok")
+# else:
+# 	print("not ok")
+# # kay = data.keys()
+# # for key, value in data.items():
+# #     print (key)
+# # key, value = data.items()
+# # print (key)
+# # from flask import Flask, render_template, redirect, url_for, request, json
+# # data['cars']["Nissan"].append({"model" + "tesst":"simp", "doors":12})
+# # print (data['cars']["Nissan"][3]  )
 
 # test = {'state': 1}
 # test.update({'cauhoi': 2})
@@ -33,58 +50,23 @@
 # test['state'] = test['state'] - 1
 # print (test)
 
-from underthesea import word_tokenize
+# 
+# from flask import Flask, render_template, redirect, url_for, request, json
+# filename = '/Users/mac/Desktop/PythonSocket/flask-chat-app-article-master/static/data/keywords.json'
+# with open(filename) as test_file:
+#     keywords = json.load(test_file)
+# word = "tesing"
+# if word in data["sth"]:
+# 	print("true")
+# else:
+# 	print ("false")
+# # for key, value in keywords["ISEaSAP"]["Học phí"][0].items():
+# #     print (key)
+# from underthesea import word_tokenize
+# string = "Trường học là nơi dùng để ngủ"
+# string = word_tokenize(string,format="text")
+# print (string)
+# string = string.lower().replace("_", " ")
+# print(string)
 
-import sys
-import numpy as np
-import os
-import re
-from keras.models import load_model
 
-#import sentimential as st
-from keras.models import model_from_json
-
-# Model reconstruction from JSON file
-with open('/Users/mac/Desktop/PythonSocket/flask-chat-app-article-master/quessmodel_architecture-final1.json', 'r') as f:
-    model = model_from_json(f.read())
-
-# Load weights into the new model
-model.load_weights('/Users/mac/Desktop/PythonSocket/flask-chat-app-article-master/quesmodel-weight-final(1).h')
-wordsList = np.load('/Users/mac/Desktop/NLP/data_train/tu.npy')
-print('Simplified vocabulary loaded!')
-#wordsList = wordsList.tolist()
-print('Size of the vocabulary: ', len(wordsList)) 
-wordsList = [word.decode('UTF-8') for word in wordsList]
-
-strip_special_chars = re.compile('[^\w0-9 ]+')
-
-def cleanSentences(string):
-    string = string.lower().replace("<br />", " ")
-    return re.sub(strip_special_chars, "", string.lower())
-
-f = ['Cho mình hỏi về vấn đề tiếng anh ở trường?']
-ids = []
-maxSeqLength = 180
-line = f[0]
-nline =  word_tokenize(cleanSentences(line), format="text")
-newline = re.sub(r'_', "", nline)
-split = newline.split()
-
-for i, word in enumerate(split):
-	if i >= maxSeqLength:
-		break
-	try:
-		idx = wordsList.index(word)
-		ids.append(idx)
-	except ValueError:
-		ids.append(518835)
-        
-ids = np.array(ids + ([0] * (maxSeqLength - len(ids))))
-ids = np.expand_dims(ids, axis=0)
-prediction = model.predict(ids)
-print (ids)
-print(prediction)
-if prediction < 0.5: 
-	print( "cmt" )
-else: 
-	print ( 'ques')
